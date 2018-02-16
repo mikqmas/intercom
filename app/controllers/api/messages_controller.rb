3f1d01f77beb57d4d90f619991af57c6980051cf
Samuel Kim <samuel.kim@clover.com>
2018-02-15 23:29:14 -0800

    message index create

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,11 @@
 class Api::MessagesController < ApplicationController
   def index
+    @messages = Message.all.order(`#{message_params.orderBy} #{message_params.direction}`).limit(message_params.limit).offset(message_params.offset)
+    if @messages
+      render json: @messages
+    else
+      render @messages.errors.full_message, status: 422
+    end
   end
 
   def show
@@ -9,8 +15,19 @@ class Api::MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.create(message_params)
+    if @message.save
+      render json: @message
+    else
+      render @message.errors.full_message, status: 422
+    end
   end
 
   def update
+  end
+
+  private
+  def message_params
+    params.permit(:body, :employee_id, :offset, :limit, :orderBy, :direction)
   end
 end
