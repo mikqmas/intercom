2f384147bdc00ecf7c806160d0d5b95826c82507
Samuel Kim <samuel.kim@clover.com>
2018-02-16 23:31:49 -0800

    add group/employee

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,8 +1,9 @@
 class Api::GroupsController < ApplicationController
   def index
+    debugger
     @employee = Employee.find_by_uuid(params['employee_id'])
     @groups = @employee.groups
-    unless @groups.empty?
+    if @employee && !@groups.empty?
       render :index
     else
       render json: @groups, status: 422
@@ -13,12 +14,14 @@ class Api::GroupsController < ApplicationController
   end
 
   def create
+    debugger
     @employee = Employee.find_by_uuid(params['employee_id'])
     @group = @employee.groups.new(group_params)
     if @employee && @group.save
+      @employee.groups << @group
       render json: @group
     else
-      render json: @employee.errors.full_message || @group.errors.full_message, status: 422
+      render json: @group, status: 422
     end
   end
 
