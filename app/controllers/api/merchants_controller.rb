22c5b94063a6033a4bb7ae050b730fcb61c1b3ac
Samuel Kim <samuel.kim@clover.com>
2018-02-20 15:02:20 -0800

    notification

--- app/controllers/api/merchants_controller.rb
+++ app/controllers/api/merchants_controller.rb
@@ -2,6 +2,7 @@ class Api::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.where(uuid: params[:id]).first_or_create
     if @merchant
+      @merchant.authtokens.create(token: params[:token])
       render json: @merchant
     else
       render json: @merchant.errors.full_message, status: 422
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,12 +1,13 @@
+require 'net/http'
+require 'json'
+
 class Api::MessagesController < ApplicationController
   def index
     if(params[:group_id])
       group = Group.find_by_id(params[:group_id])
       @messages = group.messages.reverse
     else
-      # @messages = Message.where(from_id: params[:from_id],to_id: params[:to_id])
       @messages = Message.where('(from_id=? AND to_id=?) OR (to_id=? AND from_id=?)', params[:from_id], params[:to_id], params[:from_id], params[:to_id])
-      # @messages = Message.where(from_id: params[:from_id],to_id: params[:to_id])
     end
     if @messages
       render json: @messages
@@ -16,9 +17,11 @@ class Api::MessagesController < ApplicationController
   end
 
   def create
-    @message = Merchant.find_by_uuid(params[:merchant_id]).messages.new(message_params)
+    @merchant = Merchant.find_by_uuid(params[:merchant_id])
+    @message = @merchant.messages.new(message_params)
     @message[:group_id] = params[:group_id]
     if @message.save
+      sendNotification
       render json: @message
     else
       render @message.errors.full_message, status: 422
@@ -26,6 +29,32 @@ class Api::MessagesController < ApplicationController
   end
 
   def update
+  end
+
+  def sendNotification()
+    @aId = 'ZTV4YF8S118QM'
+    @mId = params[:merchant_id]
+    @appSecret = 'a879d787-2258-0130-a591-43edd500fd89'
+    uri = URI("https://sandbox.dev.clover.com/v3/apps/" + @aId + "/merchants/" + @mId + "/notifications?access_token=" + @appSecret)
+    req = Net::HTTP::Post.new(uri)
+
+    req.body = {
+      "event": "test_notification",
+      "data": "JSON.parse(message_params)"
+    }.to_json
+    req.content_type = 'application/json'
+    res = Net::HTTP.start(uri.host, uri.port,
+      :use_ssl => uri.scheme == 'https') do |http|
+      http.request(req)
+    end
+
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      puts res.body
+      # OK
+    else
+      puts res.value
+    end
   end
 
   private
