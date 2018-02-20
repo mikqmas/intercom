a9818d8ae541bcf168eb0848c981eaf1c438f6b2
Samuel Kim <samuel.kim@clover.com>
2018-02-19 16:45:54 -0800

    new employee/group

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -2,7 +2,7 @@ class Api::GroupsController < ApplicationController
   def index
     @employee = Employee.find_by_uuid(params['employee_id'])
     @groups = @employee.groups
-    @groups = [@groups.where(name: group_params[:name])] if group_params[:name]
+    @groups = @groups.where(name: group_params[:name]) if group_params[:name]
     if @employee && !@groups.empty?
       render :index
     else
@@ -16,9 +16,12 @@ class Api::GroupsController < ApplicationController
 
   def create
     @employee = Employee.find_by_uuid(params['employee_id'])
-    @group = @employee.groups.new(group_params)
+    body = JSON.parse(params['out'])
+    name = body['name']
+    @group = @employee.groups.new(name: name)
     @employees = []
-    params['employees'].each do |employee|
+
+    body['employees'].each do |employee|
       @employees << Employee.where(uuid: employee['id'], name: employee['name'], merchant_id: Merchant.find_by_uuid(params['merchant_id']).id).first_or_create
     end
     if @employee && @group.save
