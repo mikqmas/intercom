63849d677c4f4b173ee6ac43f029a47417ae2ccd
Samuel Kim <samuel.kim@clover.com>
2018-02-20 02:19:27 -0800

    selection highlights

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -9,7 +9,6 @@ class Api::GroupsController < ApplicationController
   end
 
   def create
-    debugger
     @group = Merchant.find_by_uuid(params[:merchant_id]).groups.new(group_params)
 
     if @group.save
